bd8d92377b352a4a9aabb2ff7af3ea89c4eeeb0d
Prateek <prateekj1211@gmail.com>
2024-09-21 23:00:24 +0530

    added weighted quick union

--- UnionFind/Client.java
+++ UnionFind/Client.java
@@ -3,7 +3,7 @@ import edu.princeton.cs.algs4.StdIn;
 public class Client {
     public static void main(String[] args) {
         int N = StdIn.readInt();
-        QuickUnionUf uf = new QuickUnionUf(N);
+        WeightedQuickUnionUf uf = new WeightedQuickUnionUf(N);
 
         while(!StdIn.isEmpty()) {
             int p = StdIn.readInt();
--- UnionFind/WeightedQuickUnionUf.java
+++ UnionFind/WeightedQuickUnionUf.java
@@ -1,25 +1,42 @@
 public class WeightedQuickUnionUf implements UF {
     private int[] id;
     private int count;
+    private int[] sz;
     public WeightedQuickUnionUf(int N) {
         id = new int[N];
         for(int i = 0; i < id.length; i++) {
             id[i] = i;
         }
-
         count = N;
+        sz = new int[N];
+        for(int i = 0; i < sz.length; i++) sz[i] = 1;
     }
 
     public void union(int p, int q) {
+        int rootP = find(p);
+        int rootQ = find(q);
 
+        if(rootP == rootQ) return;
+
+        if(sz[rootP] > sz[rootQ]) {
+            id[rootQ] = rootP;
+            sz[rootP] += sz[rootQ];
+        } else {
+            id[rootP] = rootQ;
+            sz[rootQ] += sz[rootP];
+        }
+        count--;
     }
 
     public int find(int q) {
-        return 1;
+        while(id[q] != q) {
+            q = id[q];
+        }
+        return q;
     }
 
     public boolean connected(int p, int q) {
-        return false;
+        return find(p) == find(q);
     }
 
     public int count() {
