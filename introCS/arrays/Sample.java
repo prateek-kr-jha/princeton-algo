3bb0007181faa268e8f7e6cb48df7f1b630dcd9e
Prateek <prateekj1211@gmail.com>
2024-10-09 01:22:15 +0530

    Sample

--- introCS/arrays/Sample.java
+++ introCS/arrays/Sample.java
@@ -1,3 +1,24 @@
 public class Sample {
-    
+    public static void main(String[] args) {
+        int m = Integer.parseInt(args[0]);
+        int n = Integer.parseInt(args[1]);
+
+        int[] perm = new int[n];
+
+        for(int i = 0; i < n; i++) {
+            perm[i] = i;
+        }
+
+        for(int i = 0; i < n; i++) {
+            int r = i + (int) (Math.random() * (n -i));
+            int temp = perm[r];
+            perm[r] = perm[i];
+            perm[i] = temp; 
+        }
+
+        for(int i = 0; i < m; i++) {
+            System.out.print(perm[i] + " ");
+        }
+        System.out.println();
+    }
 }
