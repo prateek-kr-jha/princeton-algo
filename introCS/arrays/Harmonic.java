3049734c710b35238fd79906559eb476c1341a31
Prateek <prateekj1211@gmail.com>
2024-10-10 00:54:16 +0530

    prime sieve

---
+++ introCS/arrays/Harmonic.java
@@ -0,0 +1,5 @@
+public class Harmonic {
+    public static void main(String[] args) {
+        
+    }    
+}
--- introCS/arrays/PrimeSieve.java
+++ introCS/arrays/PrimeSieve.java
@@ -1,5 +1,27 @@
-package arrays;
-
 public class PrimeSieve {
-    
+    public static void main(String[] args) {
+        int max_number = Integer.parseInt(args[0]);
+
+        boolean[] prime_sieve = new boolean[max_number + 1];
+        int root = (int)Math.sqrt(max_number);
+        for(int i = 2; i <= max_number; i++) {
+            prime_sieve[i] = true;
+        }
+        for(int i = 2; i * i < max_number; i++) {
+            if(prime_sieve[i]) {
+                for(int j = i; i * j <= max_number; j++) {
+                    prime_sieve[i * j] = false;
+                }
+            }
+        }
+        int count =0;
+        for(int i = 2; i < max_number; i++) {
+            if(prime_sieve[i]) {
+                System.out.print(i + " ");
+                count++;
+            }
+        }
+        System.out.println();
+        System.out.println(count +" number of primes ------");
+    }
 }
