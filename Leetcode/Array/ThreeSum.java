caa9ea3cb9a278584d0d638c03a431c589e6e1ff
Prateek <prateekj1211@gmail.com>
2024-10-21 23:53:50 +0530

    started solving leetcode

---
+++ Leetcode/Array/ThreeSum.java
@@ -0,0 +1,20 @@
+import java.lang.reflect.Array;
+import java.util.*;
+
+public class ThreeSum {
+    public static List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> threeSum = new ArrayList<>();
+        
+        return threeSum;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {-1,0,1,2,-1,-4};
+
+        List<List<Integer>> threeSum = threeSum(nums);
+
+        
+    
+        System.out.println(threeSum);
+    }
+}
