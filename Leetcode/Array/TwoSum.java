643df5892cafa8c8b2ec5b797b45736c6d405cb3
Prateek <prateekj1211@gmail.com>
2024-10-27 13:09:18 +0530

    two sum hash map optimised code

--- Leetcode/Array/TwoSum.java
+++ Leetcode/Array/TwoSum.java
@@ -4,17 +4,24 @@ public class TwoSum {
     public static int[] twoSum(int[] nums, int target) {
         int[] sol = new int[2];
         HashMap<Integer, Integer> map = new HashMap<>();
+        int curr = 0;
         for(int i = 0; i < nums.length; i++) {
+            curr = target - nums[i];
+            if(map.containsKey(curr)) {
+                sol[0] = map.get(curr);
+                sol[1] = i;
+                return sol;
+            }
             map.put(nums[i], i);
         }
-        for(int i = 0; i < nums.length; i++) {
-            int newTarget = target - nums[i];
-            if(map.containsKey(newTarget) && map.get(newTarget) != i) {
-                sol[0] = i;
-                sol[1] = map.get(newTarget);
-                break;
-            } 
-        }
+        // for(int i = 0; i < nums.length; i++) {
+        //     int newTarget = target - nums[i];
+        //     if(map.containsKey(newTarget) && map.get(newTarget) != i) {
+        //         sol[0] = i;
+        //         sol[1] = map.get(newTarget);
+        //         break;
+        //     } 
+        // }
         return sol;
     }
     public static void main(String[] args) {
