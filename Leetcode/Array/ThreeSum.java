56fa07348706b6c43862f66bf03998a9585829e3
Prateek <prateekj1211@gmail.com>
2024-10-29 22:37:44 +0530

    did stuff

--- Leetcode/Array/ThreeSum.java
+++ Leetcode/Array/ThreeSum.java
@@ -2,12 +2,52 @@ import java.lang.reflect.Array;
 import java.util.*;
 
 public class ThreeSum {
-    public static List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> threeSum = new ArrayList<>();
+    public static int search(int[] nums, int a) {
+        int i = 0;
+        int j = nums.length - 1;
 
-        return threeSum;
+        while(i <= j) {
+            int curr = (i + j) / 2;
+            System.out.println(curr + "-----------" + i + " " + j);
+            if(nums[curr] == a) {
+                return curr;
+            } else if(nums[curr] > a) {
+                j = curr - 1;
+            } else {
+                i = curr + 1;
+            }
+        }
+        return -1;
     }
 
+    public static List<List<Integer>> twoSum(int[] nums, int target, int j) {
+        List<List<Integer>> sol = new ArrayList<>();
+        Arrays.sort(nums);
+        int curr = 0;
+        for(int i = 0; i < nums.length / 2; i++) {
+            curr = target - nums[i];
+            int idx = search(nums, curr); 
+            if(idx > -1){
+                List<Integer> l1 = new ArrayList<>();
+                l1.add(i);
+                l1.add(idx);
+                l1.add(j);
+                sol.add(l1);
+            }
+        }
+        return sol;
+    }
+    // public static void main(String[] args) {
+    //     int[] nums = {-1,0,5, -6};
+    //     Arrays.sort(nums);
+    //     System.out.println(search(nums, 10));
+    //     List<List<Integer>> twoSum = twoSum(nums, -1);
+
+    //     // for(int a: nums) {
+    //     //     System.out.println(a);
+    //     // }
+    //     System.out.println(twoSum);
+    // }
     public static void main(String[] args) {
         int[] nums = {-1,0,1,2,-1,-4};
         for(int x: nums) {
@@ -15,13 +55,18 @@ public class ThreeSum {
         }
         System.out.println();
         Arrays.sort(nums);
-        for(int x: nums) {
-            System.out.print(x + " ");
+        // for(int x: nums) {
+        //     System.out.print(x + " ");
+        // }
+        List<List<Integer>> sol = new ArrayList<>();
+
+        for(int i = 0; i < num.length/2; i++) {
+            sol 
         }
-        // List<List<Integer>> threeSum = threeSum(nums);
+
 
         
     
-        System.out.println();
+        System.out.println(sol);
     }
 }
--- Leetcode/Array/TwoSum.java
+++ Leetcode/Array/TwoSum.java
@@ -1,7 +1,10 @@
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
+import java.util.ArrayList;
 
 public class TwoSum {
-    public static int[] twoSum(int[] nums, int target) {
+    public static int[] twoSumHashMap(int[] nums, int target) {
         int[] sol = new int[2];
         HashMap<Integer, Integer> map = new HashMap<>();
         int curr = 0;
@@ -24,13 +27,50 @@ public class TwoSum {
         // }
         return sol;
     }
-    public static void main(String[] args) {
-        int[] num = {3,2,4};
 
-        int[] twoSum = twoSum(num, 6);
+    public static int search(int[] nums, int a) {
+        int i = 0;
+        int j = nums.length - 1;
 
-        for(int a: twoSum) {
-            System.out.println(a);
+        while(i <= j) {
+            int curr = (i + j) / 2;
+            System.out.println(curr + "-----------" + i + " " + j);
+            if(nums[curr] == a) {
+                return curr;
+            } else if(nums[curr] > a) {
+                j = curr - 1;
+            } else {
+                i = curr + 1;
+            }
         }
+        return -1;
+    }
+
+    public static List<List<Integer>> twoSum(int[] nums, int target) {
+        List<List<Integer>> sol = new ArrayList<>();
+        Arrays.sort(nums);
+        int curr = 0;
+        for(int i = 0; i < nums.length / 2; i++) {
+            curr = target - nums[i];
+            int idx = search(nums, curr); 
+            if(idx > -1){
+                List<Integer> l1 = new ArrayList<>();
+                l1.add(i);
+                l1.add(idx);
+                sol.add(l1);
+            }
+        }
+        return sol;
+    }
+    public static void main(String[] args) {
+        int[] nums = {-1,0,5, -6};
+        Arrays.sort(nums);
+        System.out.println(search(nums, 10));
+        List<List<Integer>> twoSum = twoSum(nums, -1);
+
+        // for(int a: nums) {
+        //     System.out.println(a);
+        // }
+        System.out.println(twoSum);
     }
 }
